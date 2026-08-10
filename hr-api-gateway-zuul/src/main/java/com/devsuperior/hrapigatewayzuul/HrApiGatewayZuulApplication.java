c318a359d61cab6510a8415f27ca5079103fdde6
Walyson Scarazzati da Silva <walyson.scarazzati@gmail.com>
2026-08-10 12:19:29 +0100

    24. Criando projeto hr-api-gateway-zuul

---
+++ hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -0,0 +1,15 @@
+package com.devsuperior.hrapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class HrApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatewayZuulApplication.class, args);
+	}
+
+}
